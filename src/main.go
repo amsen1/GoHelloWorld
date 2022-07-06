60405cb7e908c01360f896d2e8231d0ed483567d
Amit Sengupta <amit.ksengupta@gmail.com>
2022-07-06 16:11:22 +0200

    github ref values changes added

--- src/main.go
+++ src/main.go
@@ -20,7 +20,6 @@ func Run(log *log.Logger) {
 }
 
 // main
-//more changes
 func main() {
 	log := *log.New(os.Stdout, "go-hello-world: ", log.LstdFlags)
 	fmt.Println("Initializing GoHelloWorld!!")
